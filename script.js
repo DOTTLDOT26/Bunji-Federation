61e31541e20b8799fd6aebcf9f650ff818b5b570
DOTTLDOT26 <154114132+DOTTLDOT26@users.noreply.github.com>
2024-09-17 17:49:37 +1000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// This script handles modal behavior for the citizenship form
-window.onclick = function(event) {
-    const modal = document.getElementById('formModal');
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
